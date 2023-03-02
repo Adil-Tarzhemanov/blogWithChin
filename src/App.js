25ae649eb28deb66d7aa3f648d1826835a36fcd2
chinakkk <chingizkeldibekov@gmail.com>
2023-03-02 20:29:18 +0600

    222222

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>ADILGOD</h1>
-     <h2>CHINALOX</h2>
-    </div>
+      <div className="App">
+        <h1>ADILGOD</h1>
+        <h2>CHINALOX</h2>
+        <h3>fsdsdfsddfbdfg</h3>
+        <h4>chinPush11111</h4>
+        <h5>222222</h5>
+
+      </div>
   );
 }
 
