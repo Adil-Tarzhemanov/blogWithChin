0f0992c8500b2e020ba570fa071c80f31de554b3
adiltarzhemanov <mr.tarzhemanov@mail.ru>
2023-03-02 19:03:01 +0600

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      //new commit 2
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editd <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>ADILGOD</h1>
     </div>
   );
 }
