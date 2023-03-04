82efc7e142ee538cd3e226881079620a6984ed51
adiltarzhemanov <mr.tarzhemanov@mail.ru>
2023-03-04 21:23:57 +0600

    visial funcionality for delete and change post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./Pages/Home/Home"
+
 function App() {
   return (
       <Home/>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,37 +4,38 @@ import CreatePost from "../../components/CreatePost/CreatePost";
 import {PostsList} from "../../components/PostsLists/PostsList";
 
 const Home = () => {
-  const [createPostIsOpen, setCreatePostIsOpen] = React.useState(false);
-  const [postItems, setPostItems] = React.useState([
-    {
-      id: 1,
-      title: 'Чингиз Кельдибеков',
-      text: 'Чингиз всегда был весёлым парнем, пока не полюбил прогу, она беспощадна и холодна, одна её частичка GIT, чего только стоит...'
-    },
-    {
-      id: 2,
-      title: 'Роберт Хуясаки',
-      text: 'Роберт всегда был весёлым парнем, пока не полюбил члены, они беспощадны и холодны, одни их частички кончи, чего только стоят...'
-    }
-  ]);
-
-  const onClickCreatePost = () => {
-    setCreatePostIsOpen(prevState => !prevState)
-  }
-
-  return (
-      <div className={styles.container}>
+    const [createPostIsOpen, setCreatePostIsOpen] = React.useState(false);
+    const [postItems, setPostItems] = React.useState([
         {
-            createPostIsOpen && <CreatePost
-                setCreatePostIsOpen={setCreatePostIsOpen}
-                setPostItems={setPostItems}
-                postItems={postItems}
-            />
+            id: 1,
+            title: 'Чингиз Кельдибеков',
+            text: 'Чингиз всегда был весёлым парнем, пока не полюбил прогу, она беспощадна и холодна, одна её частичка GIT, чего только стоит...'
+        },
+        {
+            id: 2,
+            title: 'Роберт Хуясаки',
+            text: 'Роберт всегда был весёлым парнем, пока не полюбил члены, они беспощадны и холодны, одни их частички кончи, чего только стоят...'
         }
+    ]);
 
-        <button onClick={onClickCreatePost}>ПОСТ</button>
-        <PostsList postItems={postItems}/>
-      </div>
-  );
+    const onClickCreatePost = () => {
+        setCreatePostIsOpen(prevState => !prevState);
+    }
+
+    return (
+        <div className={styles.container}>
+            {
+                createPostIsOpen && <CreatePost
+                    setCreatePostIsOpen={setCreatePostIsOpen}
+                    setPostItems={setPostItems}
+                    postItems={postItems}
+                />
+            }
+
+            <button onClick={onClickCreatePost}>ПОСТ</button>
+            <PostsList postItems={postItems}/>
+        </div>
+    );
 }
+
 export default Home;
